96063d29ac0d7af788db0a0dd3ab2dacdabd67c2
davidix <d.bereihi@gmail.com>
2022-02-26 21:15:59 +0330

    remove comments

--- app/Post.php
+++ app/Post.php
@@ -6,11 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    // Table Name
     protected $table = 'posts';
-    // Primary Key
     public $primaryKey = 'id';
-    // Timestamps
     public $timestamps = true;
 
     
