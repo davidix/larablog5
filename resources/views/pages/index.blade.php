0a9bfde7c916e48726eae3d7750c23adff327524
davidix <d.bereihi@gmail.com>
2022-03-31 00:13:30 +0430

    update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
     <div class="jumbotron text-center">
-        <h1>Welcome To Laravel!</h1>
-        <p>This is the laravel application with CRUD functionality and Registration</p>
+        <h1>Welcome To Laravel Blog</h1>
+        <p>This is the laravel application with CRUD functionality and Registration<br>Awesome Project</p>
+
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
     </div>
 @endsection
